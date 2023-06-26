c1706f7cb3d7f14b08d9844b32eaa84333f69bd0
Kevin Jay <kjayh@unal.edu.co>
2023-06-25 19:47:13 -0500

    Auth feature

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/DashboardView.vue'
 import InfoFin from '../views/InfoFinView.vue'
+import Login from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/info',
       name: 'info',
       component: InfoFin
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
